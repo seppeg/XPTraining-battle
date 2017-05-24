7da8dd833995f7d1a556e858ef8c2365944bda3d
seppeg <seppeg@users.noreply.github.com>
2017-05-24 10:42:34 +0200

    failure test

--- src/test/java/be/cegeka/battle/SoldierTest.java
+++ src/test/java/be/cegeka/battle/SoldierTest.java
@@ -11,6 +11,7 @@ public class SoldierTest {
         Soldier soldier = new Soldier("name");
 
         assertThat(soldier.getName()).isEqualTo("name");
+        assertThat(false).isEqualTo(true);
     }
 
     @Test(expected = IllegalArgumentException.class)
