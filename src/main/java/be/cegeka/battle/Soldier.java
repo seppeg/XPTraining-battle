87566eb887794c019caafca33fca2bf5a06a0a63
seppeg <seppeg@users.noreply.github.com>
2017-05-24 10:13:31 +0200

    Added weapon
    Added weapon to soldier...

--- src/main/java/be/cegeka/battle/Soldier.java
+++ src/main/java/be/cegeka/battle/Soldier.java
@@ -1,12 +1,14 @@
 package be.cegeka.battle;
 
-import org.apache.commons.lang3.Validate;
-
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+import org.apache.commons.lang3.Validate;
 
 public class Soldier {
 
     private String name;
+
+    private String weapon;
 
     public Soldier(String name) {
         Validate.isTrue(isNotBlank(name));
@@ -16,5 +18,10 @@ public class Soldier {
 
     String getName() {
         return this.name;
+    }
+
+
+    public String getWeapon() {
+        return weapon;
     }
 }
